be3efcb2dac71b3e1980c0d01137d39af4519238
derekaguirre <derekaguirre@me.com>
2021-06-11 20:15:53 -0600

    Added arrow function to draw_shapes.c and functionality for the arrow in uimain.c

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,15 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
-
+// Print arrow.
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  print_square(8, size);
+    }
