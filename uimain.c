08f2225d305098e7020c56815cedce443040b788
derekaguirre <derekaguirre@me.com>
2021-06-11 21:53:40 -0600

    Added functionality for 5x7 character printing in uimain.c (menu entry 'd')

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a Upper case chars = c, Lower case chars = d) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -32,6 +32,8 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'd':
       for (char c = 'A'; c < 'D'; c++)
 	print_char_5x7(c);
       break;
